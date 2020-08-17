c5f17f783be5c2e057c0ebe01af7463417b27cd2
liuxiaohang <283700113@qq.com>
2020-08-18 00:04:55 +0800

    1

--- toy-react.js
+++ toy-react.js
@@ -1,3 +1,10 @@
+/**
+ * 为了使所有的类组件和原生标签实现 appendChild setAttribute 等方法
+ * 类组件需要继承与一个基类
+ * 在 createElement 创建虚拟dom的时候需要将 document.createElement装换为创建的ElementWrapper基类
+ * 统一实现方法，统一劫持dom创建渲染过程
+ */
+
 // 基类，所有的 类组件 都要继承
 export class Component {
   constructor() {
@@ -19,6 +26,7 @@ export class Component {
   }
 }
 
+// element类
 class ElementWrapper {
   constructor(type) {
     this.root = document.createElement(type);
@@ -31,6 +39,7 @@ class ElementWrapper {
   }
 }
 
+// 文本类
 class TextWrapper {
   constructor(type) {
     this.root = document.createTextNode(type);
