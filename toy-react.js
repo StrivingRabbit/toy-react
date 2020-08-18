23907353bca9372bcb55517c84f6a04a3e0f134c
liuxiaohang <283700113@qq.com>
2020-08-18 23:31:50 +0800

    dom渲染完善，使用range 添加setState，重渲染完善 react官网小游戏实现

--- toy-react.js
+++ toy-react.js
@@ -1,9 +1,15 @@
 /**
+ * day one
  * 为了使所有的类组件和原生标签实现 appendChild setAttribute 等方法
  * 类组件需要继承与一个基类
  * 在 createElement 创建虚拟dom的时候需要将 document.createElement装换为创建的ElementWrapper基类
  * 统一实现方法，统一劫持dom创建渲染过程
+ * 
+ * day tow
+ * 使用range Api去操作dom，只去更新当前组件设计到的一块儿dom，进行一个删除，插入，替换
  */
+
+const RENDER_TO_DOM = Symbol('render_to_dom');
 
 // 基类，所有的 类组件 都要继承
 export class Component {
@@ -11,6 +17,7 @@ export class Component {
     this.props = Object.create(null);
     this.children = [];
     this._root = null;
+    this._range = null;
   }
   appendChild(component) {
     this.children.push(component)
@@ -18,11 +25,55 @@ export class Component {
   setAttribute(key, attr) {
     this.props[key] = attr;
   }
-  get root() {
+  [RENDER_TO_DOM](range) {
+    /**
+     * this.render()
+     * 会返回一个component或者一个dom
+     * 递归去调用，知道调用到domWrapper上的 RENDER_TO_DOM 方法，渲染真实dom
+     */
+    this._range = range;
+    this.render()[RENDER_TO_DOM](range);
+  }
+  /* get root() {
+    // 递归的去拿去root属性，如果是component会一直递归调用 get root()，直到返回的是一个dom
     if (!this._root) {
       this._root = this.render().root;
     }
     return this._root;
+  } */
+  rerender() {
+    /**
+     * 在此处重绘时，在旧节点之前去重绘出新的dom
+     * 然后将旧有dom清空，这样会避免出现bug
+     */
+    let oldRange = this._range;
+
+    let range = document.createRange();
+    range.setStart(oldRange.startContainer, oldRange.startOffset);
+    range.setEnd(oldRange.startContainer, oldRange.startOffset);
+    this[RENDER_TO_DOM](range);
+
+    oldRange.setStart(range.endContainer, range.endOffset);
+    oldRange.deleteContents();
+  }
+  setState(newState) {
+    if (TypeOf(newState) != 'Object') {
+      this.state = newState;
+      return;
+    }
+    let mergeState = (oldState, newState) => {
+      for (const key in newState) {
+        if (newState.hasOwnProperty(key)) {
+          if (TypeOf(newState[key]) != 'Object') {
+            oldState[key] = newState[key];
+          } else {
+            mergeState(oldState[key], newState[key]);
+          }
+        }
+      }
+    }
+    mergeState(this.state, newState);
+    this.rerender();
   }
 }
 
@@ -30,12 +81,32 @@ export class Component {
 class ElementWrapper {
   constructor(type) {
     this.root = document.createElement(type);
+    this.state = null;
   }
   appendChild(component) {
-    this.root.appendChild(component.root);
+    // this.root.appendChild(component.root);
+    let range = document.createRange();
+    range.setStart(this.root, this.root.childNodes.length);
+    range.setEnd(this.root, this.root.childNodes.length);
+    component[RENDER_TO_DOM](range);
   }
   setAttribute(name, attr) {
-    this.root.setAttribute(name, attr);
+    if (name.match(/^on([\s\S]+)$/)) {
+      this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLocaleLowerCase()), attr);
+    } else {
+      if (name === 'className') {
+        this.root.setAttribute('class', attr);
+      } else {
+        this.root.setAttribute(name, attr);
+      }
+    }
+  }
+  [RENDER_TO_DOM](range) {
+    /**
+     * 在此处，先将range清空，然后再往range中插入新的dom
+     */
+    range.deleteContents();
+    range.insertNode(this.root);
   }
 }
 
@@ -43,6 +114,13 @@ class ElementWrapper {
 class TextWrapper {
   constructor(type) {
     this.root = document.createTextNode(type);
+  }
+  [RENDER_TO_DOM](range) {
+    /**
+     * 在此处，先将range清空，然后再往range中插入新的dom
+     */
+    range.deleteContents();
+    range.insertNode(this.root);
   }
 }
 
@@ -64,8 +142,11 @@ export function createElement(type, attrs, ...children) {
 
   const insertChildren = (children) => {
     for (let child of children) {
-      if (typeof child === 'string') {
+      if (['string', 'number'].includes(typeof child)) {
         child = new TextWrapper(child);
+      }
+      if (child === null) {
+        continue;
       }
       if (typeof child === 'object' && child instanceof Array) {
         insertChildren(child);
@@ -79,6 +160,17 @@ export function createElement(type, attrs, ...children) {
   return e;
 }
 
-export function Render(component, parentNode) {
-  parentNode.appendChild(component.root);
+export function render(component, parentNode) {
+  /**
+   * 创建一个range ，截取到 parentNode的dom范围
+   * 然后再将range范围传入到组件的 RENDER_TO_DOM中，实现往range中渲染dom操作
+   */
+  let range = document.createRange();
+  range.setStart(parentNode, 0);
+  range.setEnd(parentNode, parentNode.childNodes.length);
+  component[RENDER_TO_DOM](range);
+}
+
+function TypeOf(param) {
+  return Object.prototype.toString.call(param).match(/\s([\s\S]+)]$/)[1];
 }
